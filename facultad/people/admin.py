43a0f0e1da5ed3ac54e3ba827a9d6725a0365831
Joaquin Luberto <joaquinluberto@gmail.com>
2025-09-20 16:30:20 -0300

    Se sube el login y el register tambien incluyendo el alta del profesor

--- facultad/people/admin.py
+++ facultad/people/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ("username", "email", "rol", "legajo", "is_active", "is_staff")
+    list_filter  = ("rol", "is_active", "is_staff", "is_superuser")
+
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Rol y datos UTN", {"fields": ("rol", "legajo")}),
+    )
+    add_fieldsets = DjangoUserAdmin.add_fieldsets + (
+        ("Rol y datos UTN", {"fields": ("rol", "legajo")}),
+    )
